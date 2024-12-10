f904ddf11665a3754f55f4221a0e35a59dd0d636
bbento2 <benyawan014@gmail.com>
2024-12-10 14:21:29 +0700

    Initial commit

---
+++ Button.js
@@ -0,0 +1,5 @@
+function Submit() { /*function when you click button submit in ใบประเมิน จะมีpopup or alret ขึ้นมาข้างบนเว็บนั่นเอง*/
+    alert("Thank for Satisfaction"); /*สั่งให้แจ้งเตือนข้างบนเว็บว่า Thank for Satisfaction*/
+
+  }
+  
